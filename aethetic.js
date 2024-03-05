10363b96c0fa74112f5bef434a6b4a58d2251279
Prakriti123456 <93470873+Prakriti123456@users.noreply.github.com>
2024-03-05 18:59:30 +0530

    Add files via upload

---
+++ aethetic.js
@@ -0,0 +1,26 @@
+img = "";
+status = "";
+
+function preload() {
+    img = loadImage("aethetic");
+}
+
+function setup() {
+    canvas = createCanvas(630, 410);
+    canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+}
