09e88a6fa208ac4ffcb79799bc2f001e5a80781b
Prakriti123456 <93470873+Prakriti123456@users.noreply.github.com>
2024-03-06 16:50:46 +0530

    Add files via upload

--- fruits.js
+++ fruits.js
@@ -1,12 +1,13 @@
 img = "";
 status = "";
+objects = [];
 
 function preload() {
     img = loadImage('fruits');
 }
 
 function setup() {
-    canvas = createCanvas(630, 410);
+    canvas = createCanvas(640, 420);
     canvas.center();
 
     objectDetector = ml5.objectDetector('cocossd', modelLoaded);
@@ -18,9 +19,29 @@ function modelLoaded() {
     status = true;
     objectDetector.detect(img, gotResult);
 }
+
 function gotResult(error, results) {
     if (error) {
         console.log(error);
     }
     console.log(results);
+    objects = results;
+}
+
+function draw() {
+    image(img, 0, 0, 640, 420);
+    if (status != "") {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status :Objects Detected";
+            document.getElementById("result").innerHTML="Number of objects detected are : " + objects.length;
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            document.getElementById("result").innerHTML="Out of 1 image, No of objects detected = " + objects.length;
+        }
+    }
+
 }
--- table chair.js
+++ table chair.js
@@ -1,4 +1,6 @@
 img = "";
+status = "";
+objects = [];
 
 function preload() {
     img = loadImage('table chair');
@@ -7,13 +9,39 @@ function preload() {
 function setup() {
     canvas = createCanvas(640, 420);
     canvas.center();
+
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded() {
+    console.log("Model Loaded");
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
 }
 
 function draw() {
     image(img, 0, 0, 640, 420);
-    fill("#FF0000");
-    text("Dog", 45, 75);
-    noFill();
-    stroke("#FF0000");
-    rect(30, 60, 450, 350);
+    if (status != "") {
+        for (i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "Status :Objects Detected";
+            document.getElementById("result").innerHTML="Number of objects detected are : " + objects.length;
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + " " + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+            document.getElementById("result").innerHTML="Out of 1 image, No of objects detected = " + objects.length;
+        }
+    }
+
 }
